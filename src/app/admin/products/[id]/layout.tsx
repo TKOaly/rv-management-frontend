a13f9359a2e364722548c4ddcc650b25aef47ea3
Eetu Raunio <eetu.raunio@helsinki.fi>
2024-03-06 02:23:22 +0200

    Move product layout cross to top right

--- src/app/admin/products/[id]/layout.tsx
+++ src/app/admin/products/[id]/layout.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 async function ProductLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-full w-full flex-col items-start justify-start gap-y-4 rounded-lg border p-8 shadow-lg">
-      <div className="flex h-full w-full flex-col gap-y-4">
+      <div className="relative flex h-full w-full flex-col gap-y-4">
         <Link
           href={"/admin/products"}
-          className="-ml-3 -mt-3 w-fit rounded-md p-2 hover:bg-stone-100"
+          className="absolute right-0 top-2 -ml-3 -mr-2 -mt-3 w-fit rounded-md p-2 hover:bg-stone-100"
         >
           <X />
         </Link>
