1c481fecd145a5ac7fa0ecf0122fa66125232200
Eetu Raunio <eetu.raunio@helsinki.fi>
2024-03-08 07:44:38 +0200

    Test CRUD pipeline with random name, barcode
    This prevents collision with existing values.
    
    In the future we might want to separate these and have some fixtures to set up test products (and clean them up).

--- tests/utils/random.ts
+++ tests/utils/random.ts
@@ -1,3 +1,7 @@
 export const getRandomBarcode = async () => {
   return Math.floor(Math.random() * 1000000000).toString();
 };
+
+export const getRandomName = async () => {
+  return Math.random().toString(36).substring(12);
+};
