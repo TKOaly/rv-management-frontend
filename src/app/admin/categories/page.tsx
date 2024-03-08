5fe1d888fe2731239b495382242547dc1efe10b6
Eetu Raunio <eetu.raunio@helsinki.fi>
2024-03-08 08:13:29 +0200

    Allow creating category from category listing

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,5 +1,8 @@
+"use server";
+
 import { getAllCategories } from "@/server/requests/categoryRequests";
 import { CategoryRow } from "./CategoryRow";
+import { CreateCategoryRow } from "./CreateCategoryRow";
 
 export default async function Categories() {
   const categories = await getAllCategories();
@@ -8,12 +11,13 @@ export default async function Categories() {
     <div className="flex h-full w-full flex-col gap-y-4 py-12">
       <h1 className="text-3xl font-semibold">Categories</h1>
       <div className="h-full w-full pb-16">
-        <div className="h-full overflow-y-auto rounded-lg border shadow-lg">
+        <div className="relative h-full overflow-y-auto overscroll-none rounded-lg border shadow-lg">
           <div>
             {categories.map((category) => (
               <CategoryRow key={category.categoryId} category={category} />
             ))}
           </div>
+          <CreateCategoryRow />
         </div>
       </div>
     </div>
