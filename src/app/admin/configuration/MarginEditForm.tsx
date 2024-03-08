8e1ea7574da5ccacce3f582373b027e1bd677162
Eetu Raunio <eetu.raunio@helsinki.fi>
2024-03-08 08:35:05 +0200

    Use card component in MarginEditForm for consistency

--- src/app/admin/configuration/MarginEditForm.tsx
+++ src/app/admin/configuration/MarginEditForm.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { SubmitButton } from "@/components/ui/submit-button";
 import { useToast } from "@/components/ui/use-toast";
@@ -27,12 +34,12 @@ export const MarginEditForm = ({ defaultMargin = 0.05 }) => {
   );
   const [approximateSalesValue, setApproximateSalesValue] = useState<
     string | undefined
-  >("24760,00");
+  >("23809.52");
   const approximateSales = Number(approximateSalesValue?.replace(",", "."));
   const marginPercentage = Number(margin) / 100;
 
   const marginIncome = approximateSales * marginPercentage;
-  const difference = approximateSales * (marginPercentage - defaultMargin);
+  const difference = approximateSales * (marginPercentage - localDefaultMargin);
   const totalIncome = approximateSales * (1 + marginPercentage);
 
   const [state, changeMargin] = useFormState<
@@ -70,79 +77,102 @@ export const MarginEditForm = ({ defaultMargin = 0.05 }) => {
   }, [state.error]);
 
   return (
-    <form className="flex w-max  flex-col gap-2 rounded-lg border-2 p-3">
-      <label htmlFor="margin" className="font-semibold">
-        Default margin
-      </label>
-      <input type="hidden" value={marginPercentage} name="margin" id="margin" />
-      <Input
-        type="number"
-        step="1"
-        containerClassName="w-[15ch]"
-        className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-        value={margin}
-        onChange={({ target }) => setMargin(target.value)}
-        endSlot="%"
-        required
-      />
-
-      <div className="flex flex-col gap-2">
-        <span>Yearly approximates</span>
-        <div className="grid grid-cols-2 gap-x-6 gap-y-2 opacity-75">
-          <span>Purchases</span>
-          <span>
-            <CurrencyInput
-              value={approximateSalesValue}
-              onValueChange={(value) => setApproximateSalesValue(value)}
-              step={1}
-              decimalsLimit={2}
-              intlConfig={{ locale: "fi-FI", currency: "EUR" }}
-              decimalSeparator=","
-              groupSeparator=" "
-              className={merge(
-                "w-[10ch] rounded-sm border-b-2 transition-colors duration-200 focus:border-stone-950 focus-visible:outline-none",
-                Number.isNaN(approximateSales) &&
-                  "border-red-400 focus:border-red-700",
-              )}
+    <form>
+      <Card className="w-max">
+        <CardHeader>
+          <CardTitle>Default margin</CardTitle>
+          <CardDescription className="flex flex-col">
+            <span>
+              Defines the default percentage to use when buying in a product.
+            </span>
+            <span>
+              Does not apply to existing products and can be overridden.
+            </span>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          <input
+            type="hidden"
+            value={marginPercentage}
+            name="margin"
+            id="margin"
+          />
+          <div>
+            <Input
+              type="number"
+              aria-label="Default margin"
+              step="1"
+              containerClassName="w-[15ch]"
+              className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+              value={margin}
+              onChange={({ target }) => setMargin(target.value)}
+              endSlot="%"
+              required
             />
-          </span>
+            <span className="text-sm text-stone-500">
+              Float value: {marginPercentage}
+            </span>
+          </div>
 
-          <span>Margin</span>
-          <span className={merge(marginIncome < 0 && "text-red-500")}>
-            {Number.isNaN(marginIncome)
-              ? "-"
-              : NumberFormatter.format(marginIncome)}
-          </span>
+          <div className="flex flex-col gap-2">
+            <span className="font-semibold">Yearly approximates</span>
+            <div className="grid grid-cols-2 gap-x-6 gap-y-2 opacity-75">
+              <span>Purchases</span>
+              <span>
+                <CurrencyInput
+                  value={approximateSalesValue}
+                  onValueChange={(value) => setApproximateSalesValue(value)}
+                  step={100}
+                  decimalsLimit={2}
+                  intlConfig={{ locale: "fi-FI", currency: "EUR" }}
+                  decimalSeparator=","
+                  groupSeparator=" "
+                  className={merge(
+                    "w-[10ch] rounded-sm border-b-2 transition-colors duration-200 focus:border-stone-950 focus-visible:outline-none",
+                    Number.isNaN(approximateSales) &&
+                      "border-red-400 focus:border-red-700",
+                  )}
+                />
+              </span>
 
-          <span>Difference</span>
-          <span
-            className={merge(
-              difference < 0 && "text-red-500",
-              difference > 0 && "text-green-700",
-            )}
+              <span>Margin</span>
+              <span className={merge(marginIncome < 0 && "text-red-500")}>
+                {Number.isNaN(marginIncome)
+                  ? "-"
+                  : NumberFormatter.format(marginIncome)}
+              </span>
+
+              <span>Difference</span>
+              <span
+                className={merge(
+                  difference < 0 && "text-red-500",
+                  difference > 0 && "text-green-700",
+                )}
+              >
+                {Number.isNaN(difference)
+                  ? "-"
+                  : NumberFormatter.format(difference)}
+              </span>
+
+              <span>Total income</span>
+              <span className={merge(totalIncome < 0 && "text-red-500")}>
+                {Number.isNaN(totalIncome)
+                  ? "-"
+                  : NumberFormatter.format(totalIncome)}
+              </span>
+            </div>
+          </div>
+
+          <SubmitButton
+            id="marginSubmit"
+            className="mt-3"
+            disabled={!touched}
+            formAction={changeMargin}
           >
-            {Number.isNaN(difference)
-              ? "-"
-              : NumberFormatter.format(difference)}
-          </span>
-
-          <span>Total income</span>
-          <span className={merge(totalIncome < 0 && "text-red-500")}>
-            {Number.isNaN(totalIncome)
-              ? "-"
-              : NumberFormatter.format(totalIncome)}
-          </span>
-        </div>
-      </div>
-
-      <SubmitButton
-        id="marginSubmit"
-        className="mt-3"
-        disabled={!touched}
-        formAction={changeMargin}
-      >
-        Save
-      </SubmitButton>
+            Save
+          </SubmitButton>
+        </CardContent>
+      </Card>
     </form>
   );
 };
